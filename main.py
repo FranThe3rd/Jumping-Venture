73f04cd670205c851a5c2f482309b43510d94c5c
Francisco <figueroaf@moravian.edu>
2024-11-06 19:51:43 -0500

    added tiles

--- main.py
+++ main.py
@@ -6,18 +6,61 @@ pygame.init()
 screen_width = 1000
 screen_height = 1000
 
-screen = pygame.display.set_mode((screen_width,screen_height))
+screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Platformer")
 
-#Loading Images
+# Defining Game Variables
+tile_size = 200
+
+# Loading Images
 sun_img = pygame.image.load('img/sun.png')
 bg_img = pygame.image.load('img/sky.png')
+
+# This basically keeps running until run = False
+
+class World():
+    def __init__(self,data):
+
+        self.tile_list = []
+
+        #Load Images
+        dirt_img = pygame.image.load('img/dirt.png')
+
+        row_count = 0
+        for row in data:
+            col_count = 0
+            for tile in row:
+                if tile == 1:
+                    img = pygame.transform.scale(dirt_img, (tile_size,tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img,img_rect)
+                    self.tile_list.append(tile)
+                col_count += 1
+            row_count +=1
+
+    def draw(self):
+        for tile in self.tile_list:
+            screen.blit(tile[0],tile[1])
+
+
+world_data = [[1, 1, 1, 1, 1],
+              [1, 0, 0, 0, 1],
+              [1, 0, 0, 0, 1],
+              [1, 0, 0, 0, 1],
+              [1, 1, 1, 1, 1],]
+
+
+world = World(world_data)
 
 run = True
 while run:
 
-    screen.blit(bg_img, (0,0))
-    screen.blit(sun_img, (100,100))
+    screen.blit(bg_img, (0, 0))
+    screen.blit(sun_img, (100, 100))
+
+    world.draw()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
